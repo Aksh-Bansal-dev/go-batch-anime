8ce2b2ea78b138b9eb672e159de7fe9ab91a9669
Aksh Bansal <akshmark@gmail.com>
2022-11-08 22:34:21 +0530

    show progress while downloading

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"path"
 	"strings"
 
+	"github.com/dustin/go-humanize"
 	"github.com/gocolly/colly"
 	"github.com/joho/godotenv"
 )
@@ -20,6 +21,29 @@ var (
 	endEp      = flag.Int("end", 0, "ending episode")
 	resolution = flag.String("res", "1280", "resolution")
 )
+
+// WriteCounter counts the number of bytes written to it. It implements to the io.Writer interface
+// and we can pass this into io.TeeReader() which will report progress on each write cycle.
+type WriteCounter struct {
+	Total uint64
+}
+
+func (wc *WriteCounter) Write(p []byte) (int, error) {
+	n := len(p)
+	wc.Total += uint64(n)
+	wc.PrintProgress()
+	return n, nil
+}
+
+func (wc WriteCounter) PrintProgress() {
+	// Clear the line by using a character return to go back to the start and remove
+	// the remaining characters by filling it with spaces
+	fmt.Printf("\r%s", strings.Repeat(" ", 35))
+
+	// Return again and print current status of download
+	// We use the humanize package to print the bytes in a meaningful way (e.g. 10 MB)
+	fmt.Printf("\rDownloading... %s complete", humanize.Bytes(wc.Total))
+}
 
 func main() {
 	flag.Parse()
@@ -45,20 +69,23 @@ func main() {
 
 	c.OnHTML("div.cf-download a[href]", func(e *colly.HTMLElement) {
 		if strings.Contains(e.Text, *resolution) {
-			resp, err := http.Get(e.Attr("href"))
+			// resp, err := http.Get(e.Attr("href"))
+			// if err != nil {
+			// 	log.Fatal(err)
+			// }
+			// defer resp.Body.Close()
+			// out, err := os.Create(downloadPath)
+			// defer out.Close()
+			// if err != nil {
+			// 	log.Fatal("file error", err)
+			// }
+			// fmt.Println("Downloading at", downloadPath)
+			// _, err = io.Copy(out, resp.Body)
+
+			err := DownloadFile(homeDir, e.Attr("href"))
 			if err != nil {
 				log.Fatal(err)
 			}
-			defer resp.Body.Close()
-			// fmt.Println("Downloading", resp.Request.URL.Query().Get("title"), "...")
-			downloadPath := path.Join(homeDir, "Downloads", *animeName, resp.Request.URL.Query().Get("title"))
-			out, err := os.Create(downloadPath)
-			defer out.Close()
-			if err != nil {
-				log.Fatal("file error", err)
-			}
-			fmt.Println("Downloading at", downloadPath)
-			_, err = io.Copy(out, resp.Body)
 			fmt.Println("Complete!")
 		}
 	})
@@ -71,4 +98,44 @@ func main() {
 	for i := *startEp; i <= *endEp; i++ {
 		c.Visit(fmt.Sprintf("https://gogoanime.tel/%s-episode-%d", *animeName, i))
 	}
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory. We pass an io.TeeReader
+// into Copy() to report progress on the download.
+func DownloadFile(homeDir string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	filepath := path.Join(homeDir, "Downloads", *animeName, resp.Request.URL.Query().Get("title"))
+	fmt.Println("Downloading at", filepath)
+	// Create the file, but give it a tmp file extension, this means we won't overwrite a
+	// file until it's downloaded, but we'll remove the tmp extension once downloaded.
+	out, err := os.Create(filepath + ".tmp")
+	if err != nil {
+		return err
+	}
+
+	// Create our progress reporter and pass it to be used alongside our writer
+	counter := &WriteCounter{}
+	if _, err = io.Copy(out, io.TeeReader(resp.Body, counter)); err != nil {
+		out.Close()
+		return err
+	}
+
+	// The progress use the same line so print a new line once it's finished downloading
+	fmt.Print("\n")
+
+	// Close the file without defer so it can happen before Rename()
+	out.Close()
+
+	if err = os.Rename(filepath+".tmp", filepath); err != nil {
+		return err
+	}
+	return nil
 }
