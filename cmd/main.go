04d27861c0c82f466cc06a0e7702d6628dcf71da
Aksh Bansal <akshmark@gmail.com>
2022-11-08 23:00:54 +0530

    display download while downloading

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"path"
 	"strings"
+	"time"
 
 	"github.com/dustin/go-humanize"
 	"github.com/gocolly/colly"
@@ -25,12 +26,20 @@ var (
 // WriteCounter counts the number of bytes written to it. It implements to the io.Writer interface
 // and we can pass this into io.TeeReader() which will report progress on each write cycle.
 type WriteCounter struct {
-	Total uint64
+	Total      uint64
+	checkTime  time.Time
+	checkTotal uint64
+	bandwidth  uint64
 }
 
 func (wc *WriteCounter) Write(p []byte) (int, error) {
 	n := len(p)
 	wc.Total += uint64(n)
+	if time.Now().Sub(wc.checkTime) > time.Second {
+		wc.bandwidth = (wc.Total - wc.checkTotal) / 1024
+		wc.checkTime = time.Now()
+		wc.checkTotal = wc.Total
+	}
 	wc.PrintProgress()
 	return n, nil
 }
@@ -38,11 +47,10 @@ func (wc *WriteCounter) Write(p []byte) (int, error) {
 func (wc WriteCounter) PrintProgress() {
 	// Clear the line by using a character return to go back to the start and remove
 	// the remaining characters by filling it with spaces
-	fmt.Printf("\r%s", strings.Repeat(" ", 35))
-
+	fmt.Printf("\r%s", strings.Repeat(" ", 45))
 	// Return again and print current status of download
 	// We use the humanize package to print the bytes in a meaningful way (e.g. 10 MB)
-	fmt.Printf("\rDownloading... %s complete", humanize.Bytes(wc.Total))
+	fmt.Printf("\rDownloading... %s complete [%d KB/s]", humanize.Bytes(wc.Total), wc.bandwidth)
 }
 
 func main() {
